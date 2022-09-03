384349ddad71d11facb30681ca592876b08c82ab
Ayush Patil <97448401+LifeAyush@users.noreply.github.com>
2022-09-03 19:06:47 +0530

    Skeleton created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+
+// Page Imports
+import AboutUs from "./Pages/AboutUs/AboutUs";
+import Stakeverse from "./Pages/Stakeverse/Stakeverse";
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/stakeverse" element={<Stakeverse />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/Pages/Stakeverse/Stakeverse.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./stakeverse.css"
+const Stakeverse = () => {
+  return (
+    <div>Stakeverse</div>
+  )
+}
+
+export default Stakeverse
