982853e05bacf07d230f0259c7b742128275d3be
Ayush Patil <97448401+LifeAyush@users.noreply.github.com>
2022-09-04 17:10:04 +0530

    completed navbar mobile view and about us web view

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,63 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import { Link, useLocation } from "react-router-dom";
 import navTitle from "../../assets/images/nav-title.svg";
 import bell from "../../assets/images/bell.svg";
 import snowflake from "../../assets/images/snowflake.svg";
 import mmIcon from "../../assets/images/mm-icon.svg";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { CgClose } from "react-icons/cg";
 
 const Navbar = () => {
   const locator = useLocation();
-
+  const [open, setOpen] = useState(0);
   return (
-    <div className="navbar">
-      <div className="nav-links">
-        <Link
-          to="/"
-          className={
-            locator.pathname === "/" ? "nav-active-state" : "nav-passive-state"
-          }
-        >
-          About us
-        </Link>
-        <Link
-          to="/minimarket"
-          className={
-            locator.pathname === "/minimarket"
-              ? "nav-active-state"
-              : "nav-passive-state"
-          }
-        >
-          MiniMarket
-        </Link>
-        <Link
-          to="/stakeverse"
-          className={
-            locator.pathname === "/stakeverse"
-              ? "nav-active-state"
-              : "nav-passive-state"
-          }
-        >
-          Stakeverse
-        </Link>
-      </div>
-      <div className="nav-title">
-        <img src={navTitle} alt="Miniverse" />
-      </div>
-      <div className="nav-utils">
-        <div className="nav-utils-notification">
-          <img src={bell} alt="Notifications" />
+    <>
+      <div className="navbar">
+        <div className="nav-links">
+          <Link
+            to="/"
+            className={
+              locator.pathname === "/"
+                ? "nav-active-state"
+                : "nav-passive-state"
+            }
+          >
+            About us
+          </Link>
+          <Link
+            to="/minimarket"
+            className={
+              locator.pathname === "/minimarket"
+                ? "nav-active-state"
+                : "nav-passive-state"
+            }
+          >
+            MiniMarket
+          </Link>
+          <Link
+            to="/stakeverse"
+            className={
+              locator.pathname === "/stakeverse"
+                ? "nav-active-state"
+                : "nav-passive-state"
+            }
+          >
+            Stakeverse
+          </Link>
         </div>
-        <div className="nav-utils-coins">
-          <span className="nav-utils-coins-text">400</span>
-          <img src={snowflake} alt="Snowflakes" />
+        <div className="nav-title">
+          <img src={navTitle} alt="Miniverse" />
         </div>
-        <div className="nav-utils-user">
-          <span className="nav-utils-user-text">0xD44f...694</span>
-          <img src={mmIcon} alt="MetaMask" />
+        <div className="nav-utils">
+          <div className="nav-utils-notification">
+            <img src={bell} alt="Notifications" />
+          </div>
+          <div className="nav-utils-coins">
+            <span className="nav-utils-coins-text">400</span>
+            <img src={snowflake} alt="Snowflakes" />
+          </div>
+          <div className="nav-utils-user">
+            <span className="nav-utils-user-text">0xD44f...694</span>
+            <img src={mmIcon} alt="MetaMask" />
+          </div>
         </div>
       </div>
-    </div>
+      <div className="mobile-nav">
+        <div className="mob-nav-title">
+          <img src={navTitle} alt="Miniverse" />
+        </div>
+        <div className="mob-nav-menu" onClick={() => setOpen(!open)}>
+          {open ? <CgClose /> : <GiHamburgerMenu />}
+        </div>
+        <div className={open ? "mob-nav-menu-display" : "none"}>
+          <div className="mob-nav-links">
+            <Link
+              to="/"
+              className={
+                locator.pathname === "/"
+                  ? "nav-active-state"
+                  : "nav-passive-state"
+              }
+            >
+              About us
+            </Link>
+            <Link
+              to="/minimarket"
+              className={
+                locator.pathname === "/minimarket"
+                  ? "nav-active-state"
+                  : "nav-passive-state"
+              }
+            >
+              MiniMarket
+            </Link>
+            <Link
+              to="/stakeverse"
+              className={
+                locator.pathname === "/stakeverse"
+                  ? "nav-active-state"
+                  : "nav-passive-state"
+              }
+            >
+              Stakeverse
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -1,15 +1,71 @@
-import React from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
-import "./aboutUs.css"
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import "./aboutUs.css";
+import aboutusHero from "../../assets/images/aboutus-hero.svg";
+import aboutusGraph from "../../assets/images/aboutus-graph.svg"
+import aboutusEx from "../../assets/images/aboutus-exchange.svg"
 
 const AboutUs = () => {
   return (
-    <div className='AboutUs-container'>
-      <div className='imported-navbar'>
+    <div className="AboutUs-container">
+      <div className="imported-navbar">
         <Navbar />
       </div>
+      <div className="AboutUs-Hero">
+        <div className="AboutUs-Hero-img-div">
+          <img src={aboutusHero} alt="Globe" className="AboutUs-Hero-img-div-img"/>
+        </div>
+        <div className="AboutUs-Hero-Text-div">
+          <h1 className="AboutUs-Hero-Text-div-heading">MINIVERSE</h1>
+          <p className="AboutUs-Hero-Text-div-para">
+            Blvck Genesis is a collection of 9,999 Blvck avatar NFTs living on
+            the Ethereum blockchain. With hundreds of artistic elements, high
+            fashion traits and monochrome aesthetics, each graphic is crafted by
+            Julian O'hayon, French designer and founder of global lifestyle
+            brand,Ts living on the Ethereum blockchain. With hundreds of
+            artistic elements, high fashion traits and monochrome aesthetics,
+            each graphic is crafted by Julian O'hayon, French designer and
+            founder of global lifestyle brand,
+          </p>
+        </div>
+      </div>
+      <div className="AboutUs-main">
+        <div className="AboutUs-main-graph">
+          <img src={aboutusGraph} alt="Graph" />
+        </div>
+        <div className="AboutUs-main-stats">
+          <div className="AboutUs-main-stat-box">
+            <img src={aboutusEx} alt="Exchange" className="AboutUs-main-stat-box-img"/>
+            <div className="AboutUs-main-stat-box-text">
+              <span className="AboutUs-main-stat-box-text-title">8999</span>
+              <span className="AboutUs-main-stat-box-text-desc">Items</span>
+            </div>
+          </div>
+          <div className="AboutUs-main-stat-box">
+            <img src={aboutusEx} alt="Exchange" className="AboutUs-main-stat-box-img"/>
+            <div className="AboutUs-main-stat-box-text">
+              <span className="AboutUs-main-stat-box-text-title">8999</span>
+              <span className="AboutUs-main-stat-box-text-desc">Items</span>
+            </div>
+          </div>
+          <div className="AboutUs-main-stat-box">
+            <img src={aboutusEx} alt="Exchange" className="AboutUs-main-stat-box-img"/>
+            <div className="AboutUs-main-stat-box-text">
+              <span className="AboutUs-main-stat-box-text-title">8999</span>
+              <span className="AboutUs-main-stat-box-text-desc">Items</span>
+            </div>
+          </div>
+          <div className="AboutUs-main-stat-box">
+            <img src={aboutusEx} alt="Exchange" className="AboutUs-main-stat-box-img"/>
+            <div className="AboutUs-main-stat-box-text">
+              <span className="AboutUs-main-stat-box-text-title">8999</span>
+              <span className="AboutUs-main-stat-box-text-desc">Items</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
